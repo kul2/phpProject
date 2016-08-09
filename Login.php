9122bd6eef6383b5e6c795e95924db2d93c3fe15
kamal <kamalkul30@gmail.com>
2016-08-09 19:31:51 +0530

    Login.php changed

--- Login.php
+++ Login.php
@@ -1,14 +1,7 @@
-<html>
-   <head>
-      <title>Welcome to Login Page</title>      
-   </head>
-   
-   <body>
+
       
       <?php
-         echo "<h1>Hello, PHP!</h1>";
-		 echo "<h1> Welcome to login page</h1>";
+         echo "Hello, PHP!";
+		 echo "Welcome to login page";
       ?>
    
-   </body>
-</html>
