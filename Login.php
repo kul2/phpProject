1ce2fc664dc22aefa336f048662c957284adbeff
kamal <kamalkul30@gmail.com>
2016-08-09 19:09:34 +0530

    Initial commit

---
+++ Login.php
@@ -0,0 +1,14 @@
+<html>
+   <head>
+      <title>Welcome to Login Page</title>      
+   </head>
+   
+   <body>
+      
+      <?php
+         echo "<h1>Hello, PHP!</h1>";
+		 echo "<h1> Welcome to login page</h1>";
+      ?>
+   
+   </body>
+</html>
---
+++ UserList.php
@@ -0,0 +1,11 @@
+<html>
+   
+   <head>
+      <title>User List</title>
+   </head>
+   
+   <body>
+      <?php echo "Here are user list:!";?>
+   </body>
+
+</html>
